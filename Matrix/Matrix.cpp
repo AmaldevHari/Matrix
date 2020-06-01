fbe8b2e93b8f1e4faf0302d7268b24b1f141bbbc
amaldev@my.yorku.ca <amaldev@my.yorku.ca>
2020-05-31 21:42:41 -0400

    function definition for matrix::multiply

--- Matrix/Matrix.cpp
+++ Matrix/Matrix.cpp
@@ -7,25 +7,43 @@
 
 #include "Matrix.h"
 
+static float* assign_elements(float ary[], int row, int col){
+	float* mat;
+
+		for( int i=0 ; i< row*col ;i ++){
+
+			float* loc = mat +i;
+			*loc= ary[i];
+
+
+		}
+	return mat;
+}
+
 int main(void){
 
 
 	int row=2;int col=2;
-	float ary[row][col]={{1,0},{0,1}};
-	float* mat[row * col];
-	int r=0; int c=0;
-	for( int i=0 ; i< row*col ;i ++){
-
-		if(c == col){
-			r++;
-			c=0;
-		}
-		mat[i]= &ary[r][c];
-		c++;
-
-	}
+	float ary[]={2,0,0,1};
+	float* mat = assign_elements(ary,row,col);
+	int row2=2;
+	int col2=1;
+	float ary2[] ={1,0};
+	float* mat1 =assign_elements(ary2, row2,col2);
 	Matrix m(mat, row, col);
-   std::cout<<m.mat<<"\n" ;
+	cout<<m.mat[0]<<" "<<m.mat[1]<<"\n";
+	Matrix m2(mat1,2,1);
+	Matrix z = m.multiply(m2);
+	cout<< z.mat[0] <<" " <<z.mat[1]<< z.mat[2]<<"\n";
+
+	/*(float* a;
+	float b =5;
+	float d =10;
+	a= &b;
+	float* e=(a+ sizeof(float) );
+			e=&d;
+	cout<<a[0] <<" " <<a[1]<<endl;
+	return 0; */
 }
 
 
--- Matrix/Matrix.h
+++ Matrix/Matrix.h
@@ -18,25 +18,49 @@ public:
 
 	int row;
 	int col;
-	float** mat;
-	Matrix(float* a[], int row,int col){
+	float* mat;
+	Matrix(float* a, int row,int col){
 
 		Matrix::row = row;
 		Matrix::col = col;
-		Matrix::mat= new float* [row* col];
+		Matrix::mat= a;
 
-		for( int i=0 ; i< row*col ;i ++){
+	/*	for( int i=0 ; i< row*col ;i ++){
 
 						Matrix::mat[i]= a[i];
 
 
 					}
-
+*/
 
 	};
+	Matrix	multiply(Matrix b){
+		int col= this->col;
+		float* mat;
+		int matrix1_row= this->row;
+		int matrix2_col = b.col;
+		int matrix1_col =this->col;
+		Matrix result(mat, matrix1_row, matrix2_col);
+		int current_result_index=0;
+		for(int i=0 ;i<matrix1_row; i++){
+			for( int j=0; j<matrix2_col ;j++){
+				float entry=0;
+				for(int k=0; k< matrix1_col; k++){
+					entry+= *(this->mat + matrix1_col*i+ k)* *(b.mat + k* matrix2_col +j);
+				}
+				float* r;
+				r=mat+ current_result_index;
+				*r= entry;
+				current_result_index ++;
+			}
+		}
+
+
+		return result;
+
+		};
 
 
 };
-
 
 #endif /* MATRIX_H_ */
